25175aacd86a96232f6c77331198f43fbef3c087
Ankit Bhalla <ankit.bhalla@cars24.com>
2022-02-17 23:58:03 +0530

    Adding spring based animation
    Use of HOF
    (withRepeat & withSpring)

--- src/views/screens/reanimated/index.tsx
+++ src/views/screens/reanimated/index.tsx
@@ -3,21 +3,32 @@ import Animated, {
   useSharedValue,
   useAnimatedStyle,
   withTiming,
+  withSpring,
+  withRepeat,
 } from 'react-native-reanimated';
 import {View, StyleSheet} from 'react-native';
 import React, {useEffect} from 'react';
 
+const SIZE = 80.0;
+
 function ReactNativeReanimated() {
   const progress = useSharedValue(1);
+  const scale = useSharedValue(2);
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
       opacity: progress.value,
+      borderRadius: (progress.value * SIZE) / 2,
+      transform: [
+        {scale: scale.value},
+        {rotate: `${progress.value * 2 * Math.PI}rad`},
+      ],
     };
   }, []);
 
   useEffect(() => {
-    progress.value = withTiming(0, {duration: 5000});
+    progress.value = withRepeat(withSpring(0.5), -1, true);
+    scale.value = withRepeat(withSpring(1), -1, true);
   }, []);
 
   return (
